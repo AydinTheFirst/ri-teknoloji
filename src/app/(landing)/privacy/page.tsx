5f008288acad461ee998c97183f5a5777b9d0af0
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2025-02-02 20:39:58 +0300

    Add Privacy Policy and Terms of Service pages with styled content

---
+++ src/app/(landing)/privacy/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Privacy() {
+  return (
+    <div className="prose">
+      <h1>Gizlilik Politikası</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        bibendum, nunc ac volutpat suscipit, augue purus luctus quam, non
+        convallis dui risus at est. Integer non magna est. Suspendisse et nulla
+        et tellus interdum lobortis. Nam vestibulum, eros sed malesuada
+        fringilla, ante elit luctus lorem, a
+      </p>
+      <h2>Çerezler</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        bibendum, nunc ac volutpat suscipit, augue purus luctus quam, non
+        convallis dui risus at est. Integer non magna est. Suspendisse et nulla
+        et tellus interdum lobortis. Nam vestibulum, eros sed malesuada
+        fringilla, ante elit luctus lorem, a
+      </p>
+
+      <h2>Veri Kullanımı</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        bibendum, nunc ac volutpat suscipit, augue purus luctus quam, non
+        convallis dui risus at est. Integer non magna est. Suspendisse et nulla
+        et tellus interdum lobortis. Nam vestibulum, eros sed malesuada
+        fringilla, ante elit luctus lorem, a
+      </p>
+    </div>
+  );
+}
